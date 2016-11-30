fb12760f193e406be73199d835abc36963b0c074
jchromik <jonas.chromik@student.hpi.de>
2016-11-30 14:48:38 +0100

    add OnlyNumbers example

---
+++ OnlyNumbers.elm
@@ -0,0 +1,36 @@
+import Char exposing (isDigit)
+import String exposing (filter)
+import Html exposing (Html, input, div, text)
+import Html.Attributes exposing (placeholder)
+import Html.Events exposing (onInput)
+
+main =
+  Html.beginnerProgram { model = model, view = view, update = update }
+
+-- MODEL --
+
+type alias Model = String
+
+model : Model
+model =
+  ""
+
+-- UPDATE --
+
+type Msg =
+  Change String
+
+update : Msg -> Model -> Model
+update msg model =
+  case msg of
+    Change content ->
+      content
+
+-- VIEW --
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ input [ placeholder "plop", onInput Change ] []
+    , div [] [ text (filter isDigit model) ]
+    ]
