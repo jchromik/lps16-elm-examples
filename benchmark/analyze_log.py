e0276d9ac7477d7a4013e2d4deaa456084a2fa08
jchromik <jonas.chromik@student.hpi.de>
2016-12-13 22:52:51 +0100

    improve analyze_log.py

--- benchmark/analyze_log.py
+++ benchmark/analyze_log.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-from operator import add
+import numpy
 import re
 import sys
 
@@ -13,11 +13,21 @@ for line in logfile:
     if numpattern.match(line):
         timestamps.append(int(line))
 
+logfile.close()
+
 starts = timestamps[::2]
 ends = timestamps[1::2]
 
 runtimes = [ (e-s) for s,e in zip(starts,ends) ]
 
-print reduce(add, runtimes)/len(runtimes)
-
-logfile.close()
+print "------------------------------"
+print "count : ", len(runtimes)
+print "mean  : ", numpy.mean(runtimes)
+print "std   : ", numpy.std(runtimes)
+print "------------------------------"
+print "max (100%)     : ", numpy.max(runtimes)
+print "75% percentile : ", numpy.percentile(runtimes, 75, interpolation='midpoint')
+print "median (50%)   : ", numpy.median(runtimes)
+print "25% percentile : ", numpy.percentile(runtimes, 25, interpolation='midpoint')
+print "min (0%)       : ", numpy.min(runtimes)
+print "------------------------------"
