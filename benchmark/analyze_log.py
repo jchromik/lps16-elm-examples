292d40f00981690fcbb7285cbf2bfb33a1f403ad
jchromik <jonas.chromik@student.hpi.de>
2016-12-14 09:58:37 +0100

    refactor analyze_log.py

--- benchmark/analyze_log.py
+++ benchmark/analyze_log.py
@@ -4,30 +4,42 @@ import numpy
 import re
 import sys
 
-filename = sys.argv[1]
-logfile = open(filename, 'r')
-numpattern = re.compile('[\d]+')
-timestamps = []
+def main():
+    filename = sys.argv[1]
+    runtimes = readruntimes(filename)
+    printsummary(runtimes)
 
-for line in logfile:
-    if numpattern.match(line):
-        timestamps.append(int(line))
+def readruntimes(filename):
+    logfile = open(filename, 'r')
+    numpattern = re.compile('[\d]+')
+    timestamps = []
 
-logfile.close()
+    for line in logfile:
+        if numpattern.match(line):
+            timestamps.append(int(line))
 
-starts = timestamps[::2]
-ends = timestamps[1::2]
+    logfile.close()
 
-runtimes = [ (e-s) for s,e in zip(starts,ends) ]
+    starts = timestamps[::2]
+    ends = timestamps[1::2]
 
-print "------------------------------"
-print "count : ", len(runtimes)
-print "mean  : ", numpy.mean(runtimes)
-print "std   : ", numpy.std(runtimes)
-print "------------------------------"
-print "max (100%)     : ", numpy.max(runtimes)
-print "75% percentile : ", numpy.percentile(runtimes, 75, interpolation='midpoint')
-print "median (50%)   : ", numpy.median(runtimes)
-print "25% percentile : ", numpy.percentile(runtimes, 25, interpolation='midpoint')
-print "min (0%)       : ", numpy.min(runtimes)
-print "------------------------------"
+    return [ (e-s) for s,e in zip(starts,ends) ]
+
+def printsummary(runtimes):
+    print "------------------------------"
+    print "count : ", len(runtimes)
+    print "mean  : ", numpy.mean(runtimes)
+    print "std   : ", numpy.std(runtimes)
+    print "------------------------------"
+    print "max (100%)     : ", numpy.max(runtimes)
+    print "75% percentile : ", numpy.percentile(runtimes, 75, interpolation='midpoint')
+    print "median (50%)   : ", numpy.median(runtimes)
+    print "25% percentile : ", numpy.percentile(runtimes, 25, interpolation='midpoint')
+    print "min (0%)       : ", numpy.min(runtimes)
+    print "------------------------------"
+
+def writecsv(runtimes):
+    # TBD
+
+if __name__=="__main__":
+   main()
